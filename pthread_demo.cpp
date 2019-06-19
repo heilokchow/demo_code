6ce0345d1514f051bb0ce69ec0e9dcc4aa745a9f
heilokchow <u3514104@connect.hku.hk>
2019-06-19 19:31:21 +0800

    correct error

--- pthread_demo.cpp
+++ pthread_demo.cpp
@@ -9,44 +9,50 @@
 int ptime = 0;
 pthread_mutex_t mutex;
 
+struct cal {
+    int i;
+    int s;
+};
+
 void* function(void* arg) {
     pthread_t k = pthread_self();
-    int* a = (int*)arg;
-    int b = *a;
-    int *s = new int;
-    *s = 0;
+    cal* a = (cal*)arg;
+    int b = a->i;
+    int s = 0;
     for (int i = b; i < (b + 2); i++) {
-        *s += i;
+        s += i;
     }
+    a->s = s;
 
     pthread_mutex_lock(&mutex);
     ptime++;
-    printf("In thread1, pid = %lu, with output = %d\n", (unsigned long)k, b);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // printf("In thread1, pid = %lu, with output = %d\n", (unsigned long)k, *s);
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
     pthread_mutex_unlock(&mutex);
-    pthread_exit((void*)s);
+    // pthread_exit((void*) s);
 }
 
 int main() {
     pthread_t tid[THREADS];
     int ret;
-    int val[THREADS];
-    int a[THREADS];
-    for (int i = 0; i < THREADS; i++) a[i] = i;
+    cal val[THREADS];
+    
+    for (int i = 0; i < THREADS; i++) val[i].i = i;
     for (int i = 0; i < THREADS; i++) {
-        ret = pthread_create(&tid[i], NULL, function, (void*)&a[i]);   
+        ret = pthread_create(&tid[i], NULL, function, (void*)&val[i]);   
     }
 
-    int* s;
+    // int* s;
     for (int i = 0; i < THREADS; i++) {
-        ret = pthread_join(tid[i], (void**)& s);
-        val[i] = *s;
+        ret = pthread_join(tid[i], NULL);
+        // printf("%lu with value: %d\n", (unsigned long)s, *s);
+        // val[i] = *s;
+        // delete s;
     }
 
     for (int i = 0; i < THREADS; i++) {
-        std::cout << val[i] << '\n';
+        std::cout << val[i].s << '\n';
     }
     printf("Total threads called time: %d\n", ptime);
-    delete s;
     return 0;
 }
